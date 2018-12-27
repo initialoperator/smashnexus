74db6655e7ef0e122b91cd576b48882568ac2218
initialoperator <chrystianzhang@gmail.com>
2018-12-27 01:20:50 -0500

    complete User controller

--- src/main/java/com/zentech/smashnexus/controller/UserController.java
+++ src/main/java/com/zentech/smashnexus/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.zentech.smashnexus.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.zentech.smashnexus.model.UserDetail;
+import com.zentech.smashnexus.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value = "/getUserById/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public UserDetail getUserById(@PathVariable Long id){
+        return this.userService.getUserWithId(id);
+    }
+
+    @RequestMapping(value="/saveUser", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveUser(@RequestBody UserDetail userDetail){
+        this.userService.saveUser(userDetail);
+        return "Good!";
+    }
 }
