44797fd4fad792a16c5cdc70356b6c16d72863e6
initialoperator <chrystianzhang@gmail.com>
2018-12-29 16:05:03 -0500

    Rename repository (get ready with user class design split)

--- src/main/java/com/zentech/smashnexus/repository/UserAuthRepository.java
+++ src/main/java/com/zentech/smashnexus/repository/UserAuthRepository.java
@@ -3,7 +3,7 @@ package com.zentech.smashnexus.repository;
 import com.zentech.smashnexus.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserDetailRepository extends MongoRepository<User, String> {
+public interface UserAuthRepository extends MongoRepository<User, String> {
 
     public User findByUsername(String username);
 }
--- src/main/java/com/zentech/smashnexus/service/implementation/UserService.java
+++ src/main/java/com/zentech/smashnexus/service/implementation/UserService.java
@@ -1,12 +1,10 @@
 package com.zentech.smashnexus.service.implementation;
 
 import com.zentech.smashnexus.model.User;
-import com.zentech.smashnexus.repository.UserDetailRepository;
+import com.zentech.smashnexus.repository.UserAuthRepository;
 import com.zentech.smashnexus.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserService implements IUserService {
 
     @Autowired
-    private UserDetailRepository userDetailRepository;
+    private UserAuthRepository userAuthRepository;
 
     @Autowired
     private PasswordEncoder encoder;
@@ -24,17 +22,17 @@ public class UserService implements IUserService {
         String password = user.getPassword();
         String passwordHased = this.encoder.encode(password);
         user.setPassword(passwordHased);
-        User ud = this.userDetailRepository.save(user);
+        User ud = this.userAuthRepository.save(user);
         return ud;
     }
 
     @Override
     public User getUserWithId(String userId) {
-        return this.userDetailRepository.findById(userId).get();
+        return this.userAuthRepository.findById(userId).get();
     }
 
     @Override
     public User loadUserByUsername(String s) throws UsernameNotFoundException {
-        return this.userDetailRepository.findByUsername(s);
+        return this.userAuthRepository.findByUsername(s);
     }
 }
